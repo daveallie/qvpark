644a494b176769d586ed845bf0bf4e20dc2a49b4
Dave Allie <dave@daveallie.com>
2022-07-28 19:45:08 +1000

    Initial commit

---
+++ src/serviceworker.js
@@ -0,0 +1,19 @@
+const cachePrefix = 'pwa-qvpark';
+const version = '1.0.0';
+
+const cacheName = `${cachePrefix}-${version}`;
+
+const urlsToCache = ['/', 'https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/barcodes/JsBarcode.code128.min.js'];
+self.addEventListener('install', async (event) => {
+  await self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
